5133f24ce2629eb572e0423395f3eeb5581bdeb9
Kenneth <kenneth_wj@outlook.com>
2021-10-19 20:44:48 +0800

    test amend commit

--- test.py
+++ test.py
@@ -4,3 +4,21 @@ print('added new line')
 
 print('last commit test before going to sleep')
 print('add one more line- edit')
+
+
+data = {
+    'zxc':'123 test',
+    'fsd':'312',
+    'zxc':'313',
+    'asd':'132',
+    'zxc':'asd',
+    'zxc':'asd test',
+    'asd':'asd',
+    'asd':'132 test',
+    'asd':'asd test',
+    'asd':'132',
+    'asd':'asd',
+    'asd':':asd'
+    }
+
+# TODO Commit Ammend
